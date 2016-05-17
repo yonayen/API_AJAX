a3de8c79b5b6bca43a801b238d90d9ba609f72f5
Yonathan Ayenew <yonayen@gmail.com>
2016-05-17 08:20:55 -0700

    use 'data' variable to recieve response data from the get request

--- apps/app.js
+++ apps/app.js
@@ -1,5 +1,6 @@
 $(function() {
-	$.getJSON('www.omdbapi.com/?s=Star%20Wars&r=json', function(){
+	$.getJSON('www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
 		console.log(data);
+		// "data" is variable name used for response data from get request 
 	});
 });
