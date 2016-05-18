2736a14fe402ca02c4b920c4dd209c726fefc6d0
Yonathan Ayenew <yonayen@gmail.com>
2016-05-18 08:09:17 -0700

    experiment with 'Search'

--- apps/app.js
+++ apps/app.js
@@ -1,5 +1,6 @@
 $(function() {
-	$.getJSON('www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+	$.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+		var myData = data.search[0].Title;
 		console.log(data);
 		// "data" is variable name used for response data from get request 
 	});
