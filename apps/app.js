987ba8ca06174b199405256e059188935f29423c
Yonathan Ayenew <yonayen@gmail.com>
2016-05-18 08:15:46 -0700

    make showResults append titles to the empty div on page

--- apps/app.js
+++ apps/app.js
@@ -1,15 +1,15 @@
 $(function(){
 
   $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    var myData = data.Search;
-    $.each(myData, function(index, value){
-      console.log(value.Title);
-    });
+  	showResults(data.Search);
   });
 });
 
 function showResults(results){
-	$.each(results, function(index,value){
-		console.log(value.Title);
-	});
+  var html = "";
+  $.each(results, function(index,value){
+    html += '<p>' + value.Title + '</p>';
+    console.log(value.Title);
+  });
+  $('#search-results').html(html);
 }
