cdcf8859f566d00ad93d4df2a6caa96a25f7bc57
Yonathan Ayenew <yonayen@gmail.com>
2016-05-18 08:11:20 -0700

    iterate through Search array

--- apps/app.js
+++ apps/app.js
@@ -1,7 +1,9 @@
-$(function() {
-	$.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-		var myData = data.search[0].Title;
-		console.log(data);
-		// "data" is variable name used for response data from get request 
-	});
+$(function(){
+
+  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    var myData = data.Search;
+    $.each(myData, function(index, value){
+      console.log(value.Title);
+    });
+  });
 });
